95788fb71e56a2af39b1e99eba41be5b2284f109
imElomari <imane.elomari@etu.uae.ac.ma>
2025-01-30 22:05:44 +0100

    add auth with spring security

---
+++ ecole-gestion/src/main/java/com/example/ecolegestion/Controllers/AuthController.java
@@ -0,0 +1,35 @@
+package com.example.ecolegestion.Controllers;
+
+import com.example.ecolegestion.Entities.Utilisateur;
+import com.example.ecolegestion.Services.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class AuthController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/login")
+    public String login() {
+        return "login"; // Ensure you have a login.html in your templates folde
+        // r
+    }
+
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("utilisateur", new Utilisateur());
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerUser(@ModelAttribute Utilisateur utilisateur) {
+        userService.registerUser(utilisateur);
+        return "redirect:/login";
+    }
+}
--- ecole-gestion/src/main/java/com/example/ecolegestion/Repositories/UtilisateurRepository.java
+++ ecole-gestion/src/main/java/com/example/ecolegestion/Repositories/UtilisateurRepository.java
@@ -9,5 +9,5 @@ import java.util.Optional;
 
 @Repository
 public interface UtilisateurRepository extends JpaRepository<Utilisateur, Long> {
-    Optional<Utilisateur> findByUsername(String username);
+    Utilisateur findByUsername(String username);
 }
